0f9541dbd4f16480d0008d3a1c3eb685b47b3dd2
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-06-25 13:44:10 +0500

    loop1

---
+++ loops/01code.py
@@ -0,0 +1,6 @@
+num = [1,2,3,-4,-5,6,-7,8,9,-10]
+positive_numbers = 0
+for number in num:
+    if num>0:
+        positive_numbers+=1
+print("FInal count of positive numbers is : " , positive_numbers)
