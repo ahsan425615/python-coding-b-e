87cbbf2d7e3733af429ae64f5fe59321362357ba
Muhammad Ahsan Mushtaq <ahsanmushtaq1373@gmail.com>
2024-06-27 12:22:58 +0500

    this is function coding in python

---
+++ functions/01code.py
@@ -0,0 +1,4 @@
+def sq(num):
+    return num**2
+result = sq(4)
+print(result)
---
+++ functions/05code.py
@@ -0,0 +1,22 @@
+# for both values return
+import math
+def circle(radius):
+    area = math.pi * radius**2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
